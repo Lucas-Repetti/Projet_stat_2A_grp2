711522bd3efcb52fc8a745a4f8c75f0c78165e60
REPETTI--TUYA Lucas <id2786@clust-n2.domensai.ecole>
2025-12-27 18:48:23 +0100

    ajout

--- R/Lucas_RT_1.R
+++ R/Lucas_RT_1.R
@@ -11,6 +11,39 @@ df_sel <- df %>%
          NB_SCORE_EXTERIEUR,
          CD_CLUB)
 
+compare_scores <- function(df, row_index, threshold) {
+  # Extraire la ligne de départ
+  initial_row <- df[row_index, ]
+  cd_match <- initial_row$CD_MATCH
+  
+  # Somme initiale
+  sum_initial <- initial_row$NB_SCORE_DOMICILE + initial_row$NB_SCORE_EXTERIEUR
+  
+  # Différence initiale
+  diff_initial <- initial_row$NB_SCORE_DOMICILE - initial_row$NB_SCORE_EXTERIEUR
+  
+  last_row_index <- row_index
+  
+  # Parcourir les lignes suivantes
+  for (i in (row_index + 1):nrow(df)) {
+    if (df$CD_MATCH[i] != cd_match) {
+      break
+    }
+    
+    sum_next <- df$NB_SCORE_DOMICILE[i] + df$NB_SCORE_EXTERIEUR[i]
+    
+    if (abs(sum_next - sum_initial) > threshold) {
+      break
+    }
+    
+    last_row_index <- i
+  }
+  
+  diff_last <- df$NB_SCORE_DOMICILE[last_row_index] - df$NB_SCORE_EXTERIEUR[last_row_index]
+  
+  return(c(diff_last, diff_initial))
+}
 
+compare_scores(df_sel, row_index = 1, threshold = 5)
 
 
